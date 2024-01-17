e3e279de56bee365daa097ebf6c89f5d4b0c54c9
Ofallbrains <ofallbrains@gmail.com>
2024-01-17 05:49:32 -0800

    sql more queries

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,6 @@
--- script to run msql commands
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost
+-- creates the table force_name on your MySQL server.
+-- force_name description: id INT, name VARCHAR(256) Not Null
+-- The database name will be passed as an argument of the mysql command
+-- If the table force_name already exists, your script should not fail
+
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
