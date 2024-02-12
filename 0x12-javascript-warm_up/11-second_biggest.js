503b867bd7d8fae976f04d4608a3b89801c82620
Ofallbrains <ofallbrains@gmail.com>
2024-02-12 14:47:09 -0800

    JavaScript warmup

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that searches the second biggest integer
+// const string = process.argv.length;
+// if (string < 4) {
+//   console.log('0');
+//   } else {
+//     const counter = process.argv.sort((a, b) => a - b);
+//       console.log(counter[counter.length - 2]);
+//       }
