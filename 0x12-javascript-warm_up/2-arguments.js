aac892c79429f75f68950a4790aa7dc8b69c4814
Ofallbrains <ofallbrains@gmail.com>
2024-02-12 11:44:10 -0800

    JavaScript warmup

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const argv = process.argv;
-if (argv.length === 2) {
-	  console.log('No argument');
-} else if (argv.length === 3) {
-	  console.log('Argument found');
+const argc = process.argv.length;
+
+if (argc > 2) {
+	  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
-	  console.log('Arguments found');
+	  console.log('No argument');
 }
