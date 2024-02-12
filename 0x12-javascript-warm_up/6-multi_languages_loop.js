399c9cacfa8f77ec5524ae20bbd36d5a4fd6b5b5
Ofallbrains <ofallbrains@gmail.com>
2024-02-12 14:31:25 -0800

    JavaScript warmup

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const newlist = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let j = 0; newlist[j]; j++) {
-	  console.log(newlist[j]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+	  console.log(line);
 }
