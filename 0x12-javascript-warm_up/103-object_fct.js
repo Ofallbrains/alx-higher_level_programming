7b9a2af3622f0d87d016e6ce6dfa25995fadbca5
Ofallbrains <ofallbrains@gmail.com>
2024-02-12 12:33:25 -0800

    javaScript warmup

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
-// Update this script by adding a new function incr
-//
-// const myObject = {
-//     type: 'object',
-//         value: 12
-//           };
-//             console.log(myObject);
-//               myObject.incr = function () {
-//                   myObject.value += 1;
-//                     };
-//                       myObject.incr();
-//                         console.log(myObject);
-//                           myObject.incr();
-//                             console.log(myObject);
-//                               myObject.incr();
-//                                 console.log(myObject);
+const myObject = {
+	  type: 'object',
+	  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+	  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
