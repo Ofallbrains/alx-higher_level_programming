87e9499c32e40a8dd7086171eccf496bf88ea991
Ofallbrains <ofallbrains@gmail.com>
2024-02-12 12:50:01 -0800

    JavaScript warmup

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-const [, , ...args] = process.argv;
-
-if (args.length === 0) {
-	    console.log("No argument");
-} else if (args.length === 1) {
-	    console.log("Argument found");
-} else {
-	    console.log("Arguments found");
-}
-
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
