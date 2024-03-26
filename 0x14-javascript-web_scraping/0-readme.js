17c32079c78f7951db74cfe9b505816684440c31
Ofallbrains <ofallbrains@gmail.com>
2024-03-26 18:04:49 +0000

    webscrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
 	  if (err) {
-		      return console.log(err);
-		    }
-	  process.stdout.write(data);
+		      console.log(err);
+		    } else {
+			        console.log(data);
+			      }
 });
